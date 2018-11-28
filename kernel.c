cadfdfe3c2458013e9fb8ab5a64c4f6f5f93ca0e
Kavin Kaviarasan <kavin2016@gmail.com>
2018-11-27 21:10:59 -0500

    hi

--- kernel.c
+++ kernel.c
@@ -1,18 +1,45 @@
 #include <linux/module.h>
-#include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/unistd.h>
+#include <linux/syscalls.h>
+#include <linux/kallsyms.h>
 
-//The init function and exit function most likely has to be changed
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Yann KOETH");
+MODULE_DESCRIPTION("Loadable Kernel Module Syscall");
+MODULE_VERSION("0.1");
 
-static int init(void){
-  printk(KERN_INFO "My module is loaded."\n);
-  return 0;
+#define SYS_CALL_TABLE "sys_call_table"
+
+
+static void *original_syscall = NULL;
+
+static asmlinkage long hijackgetdents(void){
+
+return 0;
+
 }
 
-static void uninit(void){
-
-  printk(KERN_INFO "My module is unloaded. \n")
+static int init_syscall(void)
+{
+        syscall_table = (ulong *)kallsyms_lookup_name(SYS_CALL_TABLE);
+        originalGetDents=syscall_table[sys_getdents];
+        syscall_table[sys_getdents]=hijackgetdents;
+        printk(KERN_INFO "Custom syscall loaded\n");
+        return 0;
 }
 
-module_init(init);
-,module_exit(bye);
+static void cleanup_syscall(void)
+{
+        syscall_table[sys_getdents]=originalGetDents;
+        printk(KERN_INFO "Syscall at offset %d : %p\n", sys_getdents, (void *)syscall_table[sys_getdents]);
+        printk(KERN_INFO "Custom syscall unloaded\n");
+}
+
+module_init(init_syscall);
+module_exit(cleanup_syscall);
+
+
+MODULE_LICENSE("GPL v2");
+MODULE_DESCRIPTION("A kernel module to list process by their names");
